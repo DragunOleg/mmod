1693a3d4ed90f03c47454be8fff5049b1f69fa2b
dragu <dragun.oleg41@gmail.com>
2022-09-25 10:46:04 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    //plotting libs
+    implementation("org.jetbrains.lets-plot:lets-plot-batik:2.4.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.0.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
