e3a08d6c943156febf3e9de39789ad32773c7e4c
dragu <odragun@scnsoft.com>
2022-10-18 21:46:21 +0300

    Lab 2 experiment

--- src/main/kotlin/kr_two/Experiments.kt
+++ src/main/kotlin/kr_two/Experiments.kt
@@ -1,10 +1,19 @@
 package kr_two
 
+import kr_one.RandomEventGenerator
 import kr_one.plotData
 import org.apache.commons.math3.distribution.GammaDistribution
+import org.jetbrains.letsPlot.geom.geomHistogram
+import org.jetbrains.letsPlot.geom.geomVLine
+import org.jetbrains.letsPlot.ggsize
+import org.jetbrains.letsPlot.letsPlot
+import kotlin.math.ln
 
 fun main() {
-    builtGamma()
+    //remove annoying warning "Graphics2D from BufferedImage lacks BUFFERED_IMAGE hint", was actual for 1/2 PC
+    System.setProperty("org.apache.batik.warn_destination", "false")
+    //builtGamma()
+    tryErlang()
 }
 
 private fun builtGamma() {
@@ -14,4 +23,26 @@ private fun builtGamma() {
         "x" to List(n) { g.sample() }
     )
     plotData(data)
+}
+
+private fun tryErlang() {
+    val randomEventGenerator = RandomEventGenerator(isRealRandom = true, isDebug = false)
+    val n = 10
+    val mu = 2.0
+    val data = mapOf<String, List<Double>>(
+        "x" to List(10000) { smallErlang(randomEventGenerator, n, mu) }
+    )
+    plotData(data)
+    // val testBounds = data["x"]!!.sorted()
+
+//    val p = letsPlot(data) { x = "x" } + ggsize(700, 800)
+//    (p + geomHistogram(binWidth = 1)).show()
+
+}
+
+//https://www.win.tue.nl/~marko/2WB05/lecture8.pdf
+private fun smallErlang(generator: RandomEventGenerator, n: Int, mu: Double): Double {
+    var multiplicationResult = 1.0
+    val data = List(n) { generator.invoke(Pa = 1.0).randomNumber }.map { multiplicationResult *= it }
+    return -ln(multiplicationResult) / mu
 }
