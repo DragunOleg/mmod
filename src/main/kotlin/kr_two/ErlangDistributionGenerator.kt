259f06ead2ce3ed893eceacc6a6cdc1f1dc0d07e
dragu <dragun.oleg41@gmail.com>
2022-10-31 09:52:45 +0300

    lab 2 few todos done

--- src/main/kotlin/kr_two/ErlangDistributionGenerator.kt
+++ src/main/kotlin/kr_two/ErlangDistributionGenerator.kt
@@ -8,9 +8,9 @@ import org.jetbrains.letsPlot.geom.geomLine
 import org.jetbrains.letsPlot.geom.geomVLine
 import org.jetbrains.letsPlot.label.ggtitle
 import org.jetbrains.letsPlot.letsPlot
-import kotlin.math.E
-import kotlin.math.ln
-import kotlin.math.pow
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.*
 
 //todo тесты класса
 //todo посчитать кси-квадрат https://wiki.loginom.ru/articles/chi-square-test.html
@@ -19,13 +19,13 @@ fun main() {
     System.setProperty("org.apache.batik.warn_destination", "false")
 
     val erlangDistributionGenerator = ErlangDistributionGenerator(isRealRandom = true, isDebug = true)
-    val n = 10
+    val n = 7
     val mu = 2.0
-    repeat(10) {
+    repeat(3) {
         erlangDistributionGenerator(n = n, mu = mu)
     }
 
-    drawGraphs()
+    drawGraphs(n, mu)
     //invalidDataExample()
 }
 
@@ -78,17 +78,16 @@ class ErlangDistributionGenerator(
     }
 }
 
-private fun drawGraphs() {
+private fun drawGraphs(n: Int, mu: Double) {
 
-    val erlangDistributionGenerator = ErlangDistributionGenerator(isRealRandom = true, isDebug = false)
+    val erlangDistributionGenerator = ErlangDistributionGenerator(isRealRandom = false, isDebug = false)
 
-    val n = 7
-    val mu = 2.0
     val theoreticalXFrequency = 0.01
     val listTheoreticalX = List(1500) { it * theoreticalXFrequency }
-    val listTheoreticalY = listTheoreticalX.map { erlangDistributionGenerator.calculateTheoretical(n, mu, it) }
+    val listTheoreticalY = listTheoreticalX
+        .map { erlangDistributionGenerator.calculateTheoretical(n, mu, it) }
 
-    val randomValuesCount = 1000
+    val randomValuesCount = 100000
 
     val dataTheoretical = mapOf<String, List<*>>(
         "x" to listTheoreticalX,
@@ -104,7 +103,7 @@ private fun drawGraphs() {
             ) +
             //Среднее значение должно быть равно n/mu
             geomVLine(
-                xintercept = (n/mu),
+                xintercept = (n / mu),
                 color = "red",
                 linetype = "dashed"
             ))
@@ -115,9 +114,9 @@ private fun drawGraphs() {
 
     val p = letsPlot(data) { x = "x" }
     val plot = (p +
-            //geomDensity() +
-            geomHistogram(binWidth = 0.1, color="black", fill="white") { y = "..density.."} +
-            geomDensity(linetype = 1) +
+            geomHistogram(binWidth = 0.1, color = "black", fill = "white") { y = "..density.." } +
+            //default adjust is 1
+            geomDensity(linetype = 1, adjust = 0.1) +
             ggtitle(
                 "Фактическое распределение Эралнга",
                 "при n = $n; μ = $mu; размером = $randomValuesCount"
@@ -131,4 +130,79 @@ private fun drawGraphs() {
             plotTheoretical, 600, 0, 500, 700
         )
         .show()
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~Точечные оценки~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+    val mx = n / mu
+    println("Мат.ожидание = $mx")
+    val dx = data["x"]!!.average()
+    println("Несмещенная состоятельная оценка математического ожидания или выборочное среднее= $dx")
+    val s02 = 1.0 / (randomValuesCount - 1) * data["x"]!!.fold(0.0) { sum, element -> sum + (element - dx).pow(2) }
+    println("Несмещенная состоятельная оценка дисперсии =  $s02")
+    val s2 = 1.0/ randomValuesCount * data["x"]!!.fold(0.0) { sum, element -> sum + (element - dx).pow(2) }
+    println("Смещенная состоятельная оценка дисперсии = $s2")
+    val s12 = 1.0 / randomValuesCount * data["x"]!!.fold(0.0) { sum, element -> sum + (element - mx).pow(2) }
+    println("Несмещенная состоятельная оценка дисперсии = $s12")
+    val s0 = sqrt(s02)
+    println("Состоятельная оценка среднеквадратичного отклонения = $s0")
+    repeat(5) { k ->
+        val ak = 1.0 / randomValuesCount * data["x"]!!.fold(0.0) { sum, element -> sum + element.pow(k+1) }
+        val muk = 1.0 / randomValuesCount * data["x"]!!.fold(0.0) { sum, element -> sum + (element - dx).pow(k+1) }
+        println("Выборочный началный момент ${k+1}-го порядка: $ak")
+        println("Выборочный центральный момент ${k+1}-го порядка: $muk")
+    }
+    /**
+     * Метод моментов
+     */
+    val lambda = dx / s02
+    println("По методу моментов разделим оценку математического ожидания на оценку дисперсии, получим")
+    println("mu` = $lambda")
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~Интервальные оценки~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+    //todo Доверительные интервалы, страница 28
+
+    /**
+     * ~~~~~~~~~~~~~~~~~~~~~~Соответствие закона распределения распределению Эрланга~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+    //todo Критерий Колмогорова, страница 22
+    val roundedDataX: List<Double> = (plot.data!!["x"] as List<Double>)
+        //.map { it.roundToInt().toDouble() }
+        .map { BigDecimal(it).setScale(1, RoundingMode.HALF_EVEN).toDouble() } //округляем до 1 знака после запятой
+    //Сюда будем сплюсовывать density при каждом совпадении
+    val calculatedDensity = listTheoreticalY.map { 0.0 }.toMutableList()
+
+    roundedDataX.forEachIndexed { index, d ->
+        //умножаем на 10 для выравнивания с theoreticalY
+        calculatedDensity[listTheoreticalX.closestIndex(d)] += 1.0 / randomValuesCount * 10.0
+    }
+
+    val dataWeird = mapOf<String, List<*>>(
+        "x" to listTheoreticalX,
+        "y" to calculatedDensity
+    )
+    val pWeird = letsPlot(dataWeird) { x = "x"; y = "y" }
+
+    val plotWeird = (pWeird +
+            geomLine { y = "y" }).show()
+    var biggestViolation = 0 to 0.0
+    calculatedDensity.forEachIndexed { index, d ->
+        if (d != 0.0) {
+            val indexDiff = abs(d - listTheoreticalY[index])
+            if (indexDiff >= biggestViolation.second) {
+                biggestViolation = index to indexDiff
+            }
+        }
+    }
+    println("biggest violation index = ${biggestViolation.first} diff = ${biggestViolation.second}")
+    println("теоретическое значение в этом индексе x = ${listTheoreticalX[biggestViolation.first]} y = ${listTheoreticalY[biggestViolation.first]}")
+    println("экспериментальное значение после округлений = ${calculatedDensity[biggestViolation.first]}")
+
+}
+
+fun List<Double>.closestIndex(value: Double): Int {
+    val closest = minBy { abs(value - it) }
+    val result = this.indexOf(closest)
+    return result
 }
