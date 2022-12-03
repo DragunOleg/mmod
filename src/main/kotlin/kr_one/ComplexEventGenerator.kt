f8c1243709ea76658e1bc485cd2a17b9d9b9fdec
dragu <dragun.oleg41@gmail.com>
2022-12-03 11:57:21 +0300

    remake 1_2

--- src/main/kotlin/kr_one/ComplexEventGenerator.kt
+++ src/main/kotlin/kr_one/ComplexEventGenerator.kt
@@ -1,26 +1,16 @@
 package kr_one
 
 import org.jetbrains.letsPlot.GGBunch
+import org.jetbrains.letsPlot.geom.geomBar
 import org.jetbrains.letsPlot.geom.geomPoint
 import org.jetbrains.letsPlot.intern.Plot
 import org.jetbrains.letsPlot.label.ggtitle
 import org.jetbrains.letsPlot.letsPlot
-
-fun main() {
-    //remove annoying warning "Graphics2D from BufferedImage lacks BUFFERED_IMAGE hint", was actual for 1/2 PC
-    System.setProperty("org.apache.batik.warn_destination", "false")
-
-    val complexEventGenerator = ComplexEventGenerator(isRealRandom = true, isDebug = true)
-    val Pa = 0.45
-    val Pb = 0.9
-    complexEventGenerator.invoke(Pa, Pb)
-
-    drawExample()
-}
+import java.awt.FlowLayout
+import javax.swing.*
 
 class ComplexEventGenerator(
-    val isRealRandom: Boolean,
-    val isDebug: Boolean
+    val isRealRandom: Boolean, val isDebug: Boolean
 ) {
     private val randomEventGenerator = RandomEventGenerator(isRealRandom = isRealRandom, isDebug = false)
 
@@ -28,29 +18,25 @@ class ComplexEventGenerator(
         val resultA = randomEventGenerator.invoke(Pa)
         val resultB = randomEventGenerator.invoke(Pb)
 
-        val complexEventResult =
-            if (resultA.result) {
-                if (resultB.result) {
-                    Result.AB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
-                } else {
-                    Result.A_notB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
-                }
+        val complexEventResult = if (resultA.result) {
+            if (resultB.result) {
+                Result.AB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
             } else {
-                if (resultB.result) {
-                    Result.notA_B(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
-                } else {
-                    Result.notA_notB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
-                }
+                Result.A_notB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
             }
+        } else {
+            if (resultB.result) {
+                Result.notA_B(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
+            } else {
+                Result.notA_notB(Pa = Pa, x1 = resultA.randomNumber, Pb = Pb, x2 = resultB.randomNumber)
+            }
+        }
         if (isDebug) println(complexEventResult.debugString())
         return complexEventResult
     }
 
     sealed class Result(
-        val Pa: Double,
-        val x1: Double,
-        val Pb: Double,
-        val x2: Double
+        val Pa: Double, val x1: Double, val Pb: Double, val x2: Double
     ) {
         class AB(Pa: Double, x1: Double, Pb: Double, x2: Double) : Result(Pa, x1, Pb, x2)
         class A_notB(Pa: Double, x1: Double, Pb: Double, x2: Double) : Result(Pa, x1, Pb, x2)
@@ -58,54 +44,149 @@ class ComplexEventGenerator(
         class notA_notB(Pa: Double, x1: Double, Pb: Double, x2: Double) : Result(Pa, x1, Pb, x2)
 
         fun debugString(): String {
-            return ("${this.javaClass.simpleName} :" +
-                    "Pa = $Pa, x1 = $x1, Pb = $Pb, x2 = $x2")
+            return ("${this.javaClass.simpleName} :" + "Pa = $Pa, x1 = $x1, Pb = $Pb, x2 = $x2")
         }
 
         fun graphTitle(): String = this.javaClass.simpleName
     }
 }
 
-private fun drawExample() {
-    val complexEventGenerator = ComplexEventGenerator(isRealRandom = true, isDebug = false)
-    val Pa = 0.5
-    val Pb = 0.9
-    val resultList = List<ComplexEventGenerator.Result>(10000) { complexEventGenerator.invoke(Pa, Pb) }
+class KrOne2InputGetter : JFrame("Кр1, 2") {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SwingUtilities.invokeLater { KrOne2InputGetter() }
+        }
+    }
+
+    private val button = JButton("Process")
+    private val label = JLabel("Задайте ")
+
+    private val labelN = JLabel("n =")
+    private val textFieldN = JTextField(KrOneParamsSaver.loadKrOneTwoParams().n.toString(), 5)
+
+    private val labelPa = JLabel("Pa =")
+    private val textFieldPa = JTextField(KrOneParamsSaver.loadKrOneTwoParams().Pa.toString(), 5)
+    private val labelPb = JLabel("Pb =")
+    private val textFieldPb = JTextField(KrOneParamsSaver.loadKrOneTwoParams().Pb.toString(), 5)
+    private val cbRandom = JCheckBox("isRealRandom").apply {
+        isSelected = KrOneParamsSaver.loadKrOneTwoParams().realRandom
+    }
+    private val cbDebug = JCheckBox("debugLog").apply {
+        isSelected = KrOneParamsSaver.loadKrOneTwoParams().debug
+    }
+    private val cbValues = JCheckBox("showValues").apply {
+        isSelected = KrOneParamsSaver.loadKrOneTwoParams().valuesDraw
+    }
+
+    init {
+        layout = FlowLayout()
+        defaultCloseOperation = EXIT_ON_CLOSE
+        setLocationRelativeTo(null)
+        isVisible = true
+        setSize(295, 200)
+
+        button.addActionListener {
+            processButtonClick()
+        }
+
+        add(label)
+        add(labelPa)
+        add(textFieldPa)
+        add(labelPb)
+        add(textFieldPb)
+        add(labelN)
+        add(textFieldN)
+        add(cbRandom)
+        add(cbDebug)
+        add(cbValues)
+        add(button)
+    }
+
+
+    private fun processButtonClick() {
+        try {
+            val n = textFieldN.text.toInt()
+            val Pa = textFieldPa.text.toDouble()
+            val Pb = textFieldPb.text.toDouble()
+            val isRealRandom = cbRandom.isSelected
+            val isDebug = cbDebug.isSelected
+            val isValuesDraw = cbValues.isSelected
+
+            val complexEventGenerator = ComplexEventGenerator(
+                isRealRandom = isRealRandom, isDebug = isDebug
+            )
+            drawGraphs(n, Pa, Pb, complexEventGenerator, isValuesDraw)
+
+            KrOneParamsSaver.saveKrOneTwoParams(
+                KrOneTwoParams(
+                    Pa = Pa, Pb = Pb, n = n, realRandom = isRealRandom, debug = isDebug, valuesDraw = isValuesDraw
+                )
+            )
+
+
+        } catch (e: Exception) {
+            JOptionPane.showMessageDialog(
+                this@KrOne2InputGetter, """
+                    Ошибка во время процессинга:
+                    ${e.message}
+                    """.trimIndent()
+            )
+        }
+    }
+
+    private fun drawGraphs(
+        n: Int, Pa: Double, Pb: Double, complexEventGenerator: ComplexEventGenerator, valuesDraw: Boolean
+    ) {
+        val resultList = List<ComplexEventGenerator.Result>(n) { complexEventGenerator.invoke(Pa, Pb) }
+        val ABList = resultList.filterIsInstance<ComplexEventGenerator.Result.AB>().apply {
+            println("AB size = $size")
+        }
+        val notA_BList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_B>().apply {
+            println("notA_B size = $size")
+        }
+        val A_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.A_notB>().apply {
+            println("A_notB size = $size")
+        }
+        val notA_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_notB>().apply {
+            println("notA_notB size = $size")
+        }
+        val data =
+            mapOf<String, List<*>>("x" to ABList.map { "AB" } + notA_BList.map { "!AB" } + A_notBList.map { "A!B" } + notA_notBList.map { "!A!B" })
+
+        val p = letsPlot(data) { x = "x" } + ggtitle("Фактическое распределение при n = $n; Pa = $Pa, Pb = $Pb")
+        (p + geomBar()).show()
+
+        if (valuesDraw) {
+            drawExample(n, Pa, Pb, resultList)
+        }
+
+    }
+}
+
+
+private fun drawExample(n: Int, Pa: Double, Pb: Double, resultList: List<ComplexEventGenerator.Result>) {
     println("Testing resultList with Pa = $Pa, Pb = $Pb")
-    val ABList = resultList.filterIsInstance<ComplexEventGenerator.Result.AB>().apply {
-        println("AB size = $size")
-    }
-    val notA_BList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_B>().apply {
-        println("notA_B size = $size")
-    }
-    val A_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.A_notB>().apply {
-        println("A_notB size = $size")
-    }
-    val notA_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_notB>().apply {
-        println("notA_notB size = $size")
-    }
-    GGBunch()
-        .addPlot(
+    val ABList = resultList.filterIsInstance<ComplexEventGenerator.Result.AB>()
+    val notA_BList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_B>()
+    val A_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.A_notB>()
+    val notA_notBList = resultList.filterIsInstance<ComplexEventGenerator.Result.notA_notB>()
+    GGBunch().addPlot(
             plotList(ABList), 0, 350, 300, 300
-        )
-        .addPlot(
+        ).addPlot(
             plotList(notA_BList), 350, 350, 300, 300
-        )
-        .addPlot(
+        ).addPlot(
             plotList(A_notBList), 0, 0, 300, 300
-        )
-        .addPlot(
+        ).addPlot(
             plotList(notA_notBList), 350, 0, 300, 300
-        )
-        .show()
+        ).show()
 }
 
 private fun plotList(list: List<ComplexEventGenerator.Result>): Plot {
-    val data = mapOf<String, List<*>>(
-        "x1" to list.map { it.x1 },
-        "x2" to list.map { it.x2 }
-    )
+    val data = mapOf<String, List<*>>("x1" to list.map { it.x1 }, "x2" to list.map { it.x2 })
 
-    val p = letsPlot(data) { x = "x1"; y = "x2" } + ggtitle(list.firstOrNull()?.graphTitle() + " : ${list.size}") //+ ggsize(300,300)
+    val p = letsPlot(data) { x = "x1"; y = "x2" } + ggtitle(
+        list.firstOrNull()?.graphTitle() + " : ${list.size}"
+    )
     return (p + geomPoint(shape = 4))
 }
--- src/main/kotlin/kr_one/KrOneParamsSaver.kt
+++ src/main/kotlin/kr_one/KrOneParamsSaver.kt
@@ -28,6 +28,30 @@ object KrOneParamsSaver {
         }
     }
 
+    fun loadKrOneTwoParams(): KrOneTwoParams {
+        return prefNodeTwo().run {
+            KrOneTwoParams(
+                Pa = getDouble(KrOneTwoParams.PA_KEY, 0.5),
+                Pb = getDouble(KrOneTwoParams.PB_KEY, 0.5),
+                n = getInt(KrOneTwoParams.N_KEY, 10),
+                realRandom = getBoolean(KrOneTwoParams.RANDOM_KEY, true),
+                debug = getBoolean(KrOneTwoParams.DEBUG_KEY, false),
+                valuesDraw = getBoolean(KrOneTwoParams.VALUES_DRAW_KEY, true)
+            )
+        }
+    }
+
+    fun saveKrOneTwoParams(params: KrOneTwoParams) {
+        prefNodeTwo().apply {
+            putDouble(KrOneTwoParams.PA_KEY, params.Pa)
+            putDouble(KrOneTwoParams.PB_KEY, params.Pb)
+            putInt(KrOneTwoParams.N_KEY, params.n)
+            putBoolean(KrOneTwoParams.RANDOM_KEY, params.realRandom)
+            putBoolean(KrOneTwoParams.DEBUG_KEY, params.debug)
+            putBoolean(KrOneTwoParams.VALUES_DRAW_KEY, params.valuesDraw)
+        }
+    }
+
 }
 
 data class KrOneOneParams(
@@ -41,5 +65,23 @@ data class KrOneOneParams(
         const val N_KEY = "n"
         const val RANDOM_KEY = "realRandom"
         const val DEBUG_KEY = "debug"
+    }
+}
+
+data class KrOneTwoParams(
+    val Pa: Double,
+    val Pb: Double,
+    val n: Int,
+    val realRandom: Boolean,
+    val debug: Boolean,
+    val valuesDraw: Boolean
+) {
+    companion object {
+        const val PA_KEY = "Pa"
+        const val PB_KEY = "Pb"
+        const val N_KEY = "n"
+        const val RANDOM_KEY = "realRandom"
+        const val DEBUG_KEY = "debug"
+        const val VALUES_DRAW_KEY = "drawValues"
     }
 }
