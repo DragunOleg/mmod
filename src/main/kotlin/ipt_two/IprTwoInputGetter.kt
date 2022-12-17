2c251d32cfc3702c5f7b03f8fd9d87ffcac24356
dragu <dragun.oleg41@gmail.com>
2022-12-17 13:29:24 +0300

    ipr2 saving params

--- src/main/kotlin/ipt_two/IprTwoInputGetter.kt
+++ src/main/kotlin/ipt_two/IprTwoInputGetter.kt
@@ -1,4 +1,117 @@
 package ipt_two
 
-class IprTwoInputGetter {
+import java.awt.FlowLayout
+import java.lang.Exception
+import javax.swing.*
+
+class IprTwoInputGetter : JFrame("IPR2") {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //remove annoying warning "Graphics2D from BufferedImage lacks BUFFERED_IMAGE hint", was actual for 1/2 PC
+            System.setProperty("org.apache.batik.warn_destination", "false")
+
+            SwingUtilities.invokeLater { IprTwoInputGetter() }
+        }
+    }
+
+    var startButton = JButton("start")
+    var stopButton = JButton("stop")
+
+    val labelChannelsN = JLabel("n каналов =")
+    val textFieldChannelsN = JTextField(IprTwoParamsSaver.loadIprTwoParams().channelsN.toString(), 4)
+
+    val labelQueueM = JLabel("m мест в очереди =")
+    val textFieldQueueN = JTextField(IprTwoParamsSaver.loadIprTwoParams().queueM.toString(), 4)
+
+    val labelInputFlowLambda = JLabel("λ интенсивность входящего потока? =")
+    val textFieldInputFlowLambda = JTextField(IprTwoParamsSaver.loadIprTwoParams().inputFlowLambda.toString(), 4)
+
+    val labelServiceFlowMu = JLabel("μ интенсивность потока обслуживания? =")
+    val textFieldServiceFlowMu = JTextField(IprTwoParamsSaver.loadIprTwoParams().serviceFlowMu.toString(), 4)
+
+    val labelLeavingNu = JLabel("ν параметр закона ухода =")
+    val textFieldLeavingNu = JTextField(IprTwoParamsSaver.loadIprTwoParams().leavingNu.toString(), 4)
+
+    var nChannels: Int? = null
+    var mQueue: Int? = null
+    var lambdaInputFlow: Double? = null
+    var muServiceFlow: Double? = null
+    var nuLeaving: Double? = null
+
+
+    init {
+        layout = FlowLayout()
+        defaultCloseOperation = EXIT_ON_CLOSE
+        setLocationRelativeTo(null)
+        isVisible = true
+        setSize(420, 200)
+
+        startButton.addActionListener {
+            startButtonClicked()
+        }
+        stopButton.addActionListener {
+            stopButtonClicked()
+        }
+
+        add(labelChannelsN)
+        add(textFieldChannelsN)
+        add(labelQueueM)
+        add(textFieldQueueN)
+        add(labelInputFlowLambda)
+        add(textFieldInputFlowLambda)
+        add(labelServiceFlowMu)
+        add(textFieldServiceFlowMu)
+        add(labelLeavingNu)
+        add(textFieldLeavingNu)
+        add(startButton)
+        add(stopButton)
+
+        // TODO: enable and calculate theoretical without issues 
+        labelLeavingNu.disable()
+        textFieldLeavingNu.disable()
+    }
+
+    private fun startButtonClicked() {
+        try {
+            nChannels = textFieldChannelsN.text.toInt().also { println("n каналов = $it") }
+            mQueue = textFieldQueueN.text.toInt().also { println("m место в очереди = $it") }
+            lambdaInputFlow = textFieldInputFlowLambda.text.toDouble().also { println("λ вход = $it") }
+            muServiceFlow = textFieldServiceFlowMu.text.toDouble().also { println("μ обслуживания = $it") }
+            nuLeaving = textFieldLeavingNu.text.toDouble().also { println("ν параметр закона ухода = $it") }
+
+        } catch (e: Exception) {
+            JOptionPane.showMessageDialog(
+                this@IprTwoInputGetter,
+                """
+                    Ошибка во время работы системы:
+                    ${e.message}
+                    """.trimIndent()
+            )
+        }
+    }
+
+    private fun stopButtonClicked() {
+        try {
+
+            IprTwoParamsSaver.saveIprTwoParams(
+                IprTwoParams(
+                channelsN = nChannels!!,
+                queueM = mQueue!!,
+                inputFlowLambda = lambdaInputFlow!!,
+                serviceFlowMu = muServiceFlow!!,
+                leavingNu = nuLeaving!!
+            )
+            )
+
+        } catch (e: Exception) {
+            JOptionPane.showMessageDialog(
+                this@IprTwoInputGetter,
+                """
+                    Ошибка во время обработки результатов:
+                    ${e.message}
+                    """.trimIndent()
+            )
+        }
+    }
 }
--- src/main/kotlin/ipt_two/IprTwoParamsSaver.kt
+++ src/main/kotlin/ipt_two/IprTwoParamsSaver.kt
@@ -5,16 +5,46 @@ import java.util.prefs.Preferences
 object IprTwoParamsSaver {
     private fun prefNode(): Preferences = Preferences.userRoot().node("IPR_TWO")
 
+    fun loadIprTwoParams(): IprTwoParams {
+        return prefNode().run {
+            IprTwoParams(
+                channelsN = getInt(IprTwoParams.N_KEY, 3),
+                queueM = getInt(IprTwoParams.M_KEY, 10),
+                inputFlowLambda = getDouble(IprTwoParams.LAMBDA_KEY, 1.0),
+                serviceFlowMu = getDouble(IprTwoParams.MU_KEY, 1.0),
+                leavingNu = getDouble(IprTwoParams.NU_KEY, 1.0)
+            )
+        }
+    }
 
+    fun saveIprTwoParams(params: IprTwoParams) {
+        prefNode().apply {
+            putInt(IprTwoParams.N_KEY, params.channelsN)
+            putInt(IprTwoParams.M_KEY, params.queueM)
+            putDouble(IprTwoParams.LAMBDA_KEY, params.inputFlowLambda)
+            putDouble(IprTwoParams.MU_KEY, params.serviceFlowMu)
+            putDouble(IprTwoParams.NU_KEY, params.leavingNu)
+        }
+    }
 }
 
 /**
  * @param inputFlowLambda λ интенсивность входящего потока
  * @param serviceFlowMu μ интенсивность потока обслуживания
+ * @param leavingNu ν параметр закона ухода
  */
 data class IprTwoParams(
     val channelsN: Int,
     val queueM: Int,
     val inputFlowLambda: Double,
-    val serviceFlowMu: Double
-)
+    val serviceFlowMu: Double,
+    val leavingNu: Double
+) {
+    companion object {
+        const val N_KEY = "n"
+        const val M_KEY = "m"
+        const val LAMBDA_KEY = "λ"
+        const val MU_KEY = "μ"
+        const val NU_KEY = "ν"
+    }
+}
