e2fce10d43e3366169ae43a392bc7e0ebaac46b9
dragu <dragun.oleg41@gmail.com>
2023-06-17 10:40:21 +0300

    Bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.8.21"
     application
 }
 
@@ -16,16 +16,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     //plotting libs
-    implementation("org.jetbrains.lets-plot:lets-plot-batik:2.5.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.1.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-batik:3.1.0")
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.3.0")
     //testing weibull distribution
     implementation("org.apache.commons:commons-math3:3.6.1")
     //need for connecting with Excel
     implementation("org.apache.poi:poi:5.2.2")
     //https://stackoverflow.com/questions/47881821/error-statuslogger-log4j2-could-not-find-a-logging-implementation
-    implementation("org.apache.logging.log4j:log4j-to-slf4j:2.8.2")
+    implementation("org.apache.logging.log4j:log4j-to-slf4j:2.20.0")
 
-    val coroutinesVersion = "1.6.4"
+    val coroutinesVersion = "1.7.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
